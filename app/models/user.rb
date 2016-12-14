0461036bea2e6f6464e9c2bc2e3f466ed5f67b20
Dave Charnuska <davecharnuska@gmail.com>
2016-12-14 12:43:56 -0500

    add #password, #password=, begin ::find_by_credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,21 @@ class User < ActiveRecord::Base
   validates :email, :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: true
   validates :password, length: { minimum: 6, allow_nil: true }
+
+  attr_reader :password
+
+  def self.find_by_credentials(creds_hash)
+    user = User.find_by_email(cres_hash[:email])
+
+    if user
+
+    else
+      flash[:errors] = "Invalid credentials"
+    end
+  end
+
+  def password=(password)
+    puts "HIT PASSWORD=!!!!"
+    self.password_digest = BCrypt::Password.create(password)
+  end
 end
