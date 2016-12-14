7e2ed34c1b2c87624768126d2caa2bf813356509
Dave Charnuska <davecharnuska@gmail.com>
2016-12-14 12:29:25 -0500

    fix user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
-  validates :email, presence: true, uniqueness: true
-  validates :password_digest, :session_token, presence: true
-  validates :password, presence: { message: "password cannot be blank" }
-  validates :password, length: { minimum: 6, allow_nil }
+  validates :email, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
 end
