194ba5b856045854c2d144856c14ef22bcee5162
Dave Charnuska <davecharnuska@gmail.com>
2016-12-14 14:22:58 -0500

    add authenticity token

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def auth_token
+    str = "<input type='hidden'" +
+           "name='authenticity_token'" +
+           "value='#{form_authenticity_token}'>"
+
+    str.html_safe
+  end
 end
