eb87592ac15f8f33c6d1465e4ffa15046f99e9e5
Dave Charnuska <davecharnuska@gmail.com>
2016-12-14 12:43:31 -0500

    move #user_params to application controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,9 +20,4 @@ class UsersController < ApplicationController
       render json: @user.errors.full_messages
     end
   end
-
-  private
-  def user_params
-    params.require(:user).permit(:email, :password)
-  end
 end
