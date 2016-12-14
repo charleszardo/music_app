d60f5b1b090c13bd5e0a7783746f9184a1c9d14d
Dave Charnuska <davecharnuska@gmail.com>
2016-12-14 12:49:24 -0500

    add #is_password?

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,10 @@ class User < ActiveRecord::Base
     self.password_digest = BCrypt::Password.create(password)
   end
 
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
   private
   def ensure_session_token
     self.session_token ||= SecureRandom::urlsafe_base64(16)
