a8b80b16dd7b2af5968521eabc21f62a95a07f0a
Dave Charnuska <davecharnuska@gmail.com>
2016-12-16 17:37:25 -0500

    fix track form, add track show view

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,19 +1,19 @@
 class TracksController < ApplicationController
+
   def new
     @track = Track.new
 
     render :new
   end
 
-  def update
+  def create
     @track = Track.new(track_params)
 
     if @track.save
       redirect_to track_url(@track)
     else
-      flash[:errors] = @track.errors.full_messages
-
-      redirect_to new_track_url
+      @album = @track.album
+      render :new
     end
   end
 
@@ -46,8 +46,14 @@ class TracksController < ApplicationController
     end
   end
 
+  def show
+    @track = Track.find(params[:id])
+
+    render :show
+  end
+
   private
   def track_params
-    params.require(:track).permit(:title, :album, :track_type, :lyrics)
+    params.require(:track).permit(:title, :album_id, :track_type, :lyrics)
   end
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -2,4 +2,8 @@ class Track < ActiveRecord::Base
   validates :title, :album, :track_type, presence: true
 
   belongs_to :album
+
+  def bonus?
+    self.track_type == "bonus"
+  end
 end
