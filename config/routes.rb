6e5925856fabc6dd01341daaa453acb64a11b07f
Dave Charnuska <davecharnuska@gmail.com>
2016-12-16 16:34:33 -0500

    add tracks routes

--- config/routes.rb
+++ config/routes.rb
@@ -63,5 +63,9 @@ Rails.application.routes.draw do
     resources :albums, only: [:new]
   end
 
-  resources :albums, except: [:new, :index]
+  resources :albums, only: [:create, :destroy, :edit, :update] do
+    resources :tracks, only: [:new]
+  end
+
+  resources :tracks, except: [:index, :new]
 end
