06cc99b944efc6feb1cf1eb7071f7b4a71db4e25
Dave Charnuska <davecharnuska@gmail.com>
2016-12-16 12:07:58 -0500

    add track validations, associations

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,4 +2,5 @@ class Album < ActiveRecord::Base
   validates :title, :band, :live, presence: true
 
   belongs_to :band
+  has_many :tracks, dependent: :destroy
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,2 +1,5 @@
 class Track < ActiveRecord::Base
+  validates :title, :album, :track_type, presence: true
+
+  belongs_to :album
 end
