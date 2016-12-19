7d58d8dc4d479926c08e0576ca2ed9c0f0704faf
Dave Charnuska <davecharnuska@gmail.com>
2016-12-19 10:44:15 -0500

    add tags table/model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161219145330) do
+ActiveRecord::Schema.define(version: 20161219154125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,14 @@ ActiveRecord::Schema.define(version: 20161219145330) do
 
   add_index "notes", ["track_id"], name: "index_notes_on_track_id", using: :btree
   add_index "notes", ["user_id"], name: "index_notes_on_user_id", using: :btree
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "text",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "tags", ["text"], name: "index_tags_on_text", unique: true, using: :btree
 
   create_table "tracks", force: :cascade do |t|
     t.string   "title",      null: false
