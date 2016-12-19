abd0dcd63d5d406b087d6e5ccebaa76e0c0107d2
Dave Charnuska <davecharnuska@gmail.com>
2016-12-19 09:26:28 -0500

    add ugly lyrics

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,2 +1,11 @@
+include ERB::Util
+
 module TracksHelper
+  def ugly_lyrics(lyrics)
+    formatted_lyrics = ""
+    lyrics.lines.each do |line|
+      formatted_lyrics += "&#9835; #{line}"
+    end
+    "<pre>#{formatted_lyrics}</pre>".html_safe
+  end
 end
