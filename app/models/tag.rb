58b0ce30ce4f64c4afdee6e98aae13a555a3faf4
Dave Charnuska <davecharnuska@gmail.com>
2016-12-19 19:19:17 -0500

    add Tag::find_or_create_by_tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,9 @@
 class Tag < ActiveRecord::Base
   validates :text, presence: true, uniqueness: true
+
+  def self.find_or_create_by_tag(tag_name)
+    tag = Tag.find_by(text: tag_name)
+    tag = Tag.create(text: tag_name) if tag.nil?
+    tag
+  end
 end
