a43cf6352bdda70333112f10853ab43fee315c52
Dave Charnuska <davecharnuska@gmail.com>
2016-12-19 19:21:18 -0500

    add taggings, tags associations

--- app/models/band.rb
+++ app/models/band.rb
@@ -4,6 +4,8 @@ class Band < ActiveRecord::Base
 
   belongs_to :owner, class_name: "User", foreign_key: :user_id
   has_many :albums, dependent: :destroy
+  has_many :taggings, as: :taggable
+  has_many :tags, through: :taggings
 
   def is_owner?(user)
     self.owner == user
