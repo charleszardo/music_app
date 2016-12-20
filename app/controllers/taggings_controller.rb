705a8ddf12a01540d1f0bfdb0716394bfd4da184
Dave Charnuska <davecharnuska@gmail.com>
2016-12-19 19:53:55 -0500

    add Taggings#tagging_show

--- app/controllers/taggings_controller.rb
+++ app/controllers/taggings_controller.rb
@@ -3,11 +3,18 @@ class TaggingsController < ApplicationController
     @tagging = Tagging.new(tagging_params)
 
     @tagging.save
-    redirect_to root_url
+    redirect_to tagging_show
   end
 
   private
   def tagging_params
     params.require(:tagging).permit(:tag, :taggable_id, :taggable_type)
+  end
+
+  def tagging_show
+    taggable = @tagging.taggable
+    id = taggable.id
+    type = taggable.class.to_s.downcase
+    self.send("#{type}_url", id)
   end
 end
