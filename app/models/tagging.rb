bffe0110625b9a53b95ee4f061a07ffd1cb08dcd
Dave Charnuska <davecharnuska@gmail.com>
2016-12-19 19:20:47 -0500

    update tagging validations, add tagging associations, Tag#tag=

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,3 +1,11 @@
 class Tagging < ActiveRecord::Base
-  validates :tag_id, :tagging_id, :tagging_type, presence: true
+  validates :tag_id, :taggable_id, :taggable_type, presence: true
+
+  belongs_to :taggable, polymorphic: true
+  belongs_to :tag
+
+  def tag=(tag_name)
+    tag = Tag.find_or_create_by_tag
+    self.tag_id = tag.id
+  end
 end
