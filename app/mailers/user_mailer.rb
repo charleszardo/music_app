4ca2c09b7df54502e89ef455bcfb51800790ac94
Dave Charnuska <davecharnuska@gmail.com>
2016-12-21 13:22:29 -0500

    refactor user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,8 @@
 class UserMailer < ApplicationMailer
-  default from: 'davecharnuska@example.com'
+  default from: "from@example.com"
 
-  def welcome_email(user)
+  def activation_email(user)
     @user = user
-    @activate_url = "http://localhost:3000/users/activate?activation_token=#{user.activation_token}"
-    mail(to: @user.email, subject: 'Welcome to Music App')
+    mail(to: user.email, subject: "Welcome to Music App")
   end
 end
