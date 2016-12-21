303e0aae99beab5e1e199f4c01e89d04b75f8d99
Dave Charnuska <davecharnuska@gmail.com>
2016-12-20 21:02:49 -0500

    add user email html and text

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user
-    @url = 'http://example.com/login'
+    @url = 'http://localhost:3000'
     mail(to: "User <#{user.email}>", subject: 'Welcome to Music App', cc: "Dave <davecharnuska@gmail.com>")
   end
 end
