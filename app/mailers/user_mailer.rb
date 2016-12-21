5a8f72070da9c30f58d7c8f5bb3f903573263f6a
Dave Charnuska <davecharnuska@gmail.com>
2016-12-20 20:52:45 -0500

    add default from, #welcome_email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: 'music_app@example.com'
+
+  def welcome_email(user)
+    @user = user
+    @url = 'http://example.com/login'
+    mail(to: "User <#{user.email}>", subject: 'Welcome to Music App', cc: "Dave <davecharnuska@gmail.com>")
+  end
 end
